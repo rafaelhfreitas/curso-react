47c4785e24f68f3a32f82168043f72f55367b7d5
Rafael Freitas <oraffaudi@gmail.com>
2020-09-07 21:36:44 -0300

    CSS applitcation on project / create card component / children components

---
+++ src/components/layout/Card.jsx
@@ -0,0 +1,14 @@
+import './Card.css'
+import React from 'react'
+
+export default function Card(props) {
+    return (
+        <div className='Card'>
+            <div className='Title'> { props.titulo }</div>
+            <div className='Content'>
+                {props.children}
+            </div>
+ 
+        </div>
+    )
+}
