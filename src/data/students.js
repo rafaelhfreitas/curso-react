265e320de439a59650d3f283643cb3216a73147f
Rafael Freitas <oraffaudi@gmail.com>
2020-09-08 10:09:09 -0300

    Component with repetition

---
+++ src/data/students.js
@@ -0,0 +1,11 @@
+export default [
+    { id: 1, name: 'Rafael',  grade: 9.2 },
+    { id: 2, name: 'Henrique',  grade: 9 },
+    { id: 3, name: 'Masa',  grade: 10 },
+    { id: 4, name: 'Viviane',  grade: 8.0 },
+    { id: 5, name: 'Rafael',  grade: 3.4 },
+    { id: 6, name: 'Rafael',  grade: 6.0 },
+    { id: 7, name: 'Rafael',  grade: 7.5 },
+    { id: 8, name: 'Rafael',  grade: 5.7 },
+    { id: 9, name: 'Rafael',  grade: 910 },
+]
