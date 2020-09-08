7bc5489eced69f05eb4068ae29e74aa0a194c979
Rafael Freitas <oraffaudi@gmail.com>
2020-09-07 23:02:30 -0300

    Refactoring translate to english

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 
 import Card from './components/layout/Card'
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
-import Aleatorio from './components/basicos/Aleatorio'
+import First from './components/basicos/First'
+import Fragment from './components/basicos/Fragment'
+import Random from './components/basicos/Random'
+import WithParameter from './components/basicos/WithParameter'
 
 export default (props) => {
     return (
@@ -14,20 +14,23 @@ export default (props) => {
             <h1> Fundamentos React </h1>
             
             <div  className='Cards'>
-                <Card titulo='#04 - Desafio aleatorio'>
-                    <Aleatorio min={1} max={60} />
+                <Card titulo='#05 - Componente com os filhos' color="#FA6900">
+                    
                 </Card>
-                <Card titulo='#03 - Fragmento'>
-                    <Fragmento/>
+                <Card titulo='#04 - Desafio aleatorio' color="#FA6900">
+                    <Random min={1} max={60} />
                 </Card>
-                <Card titulo='#02 - Com parâmetro'>
-                    <ComParametro 
+                <Card titulo='#03 - Fragmento' color='#E94C6F'>
+                    <Fragment/>
+                </Card>
+                <Card titulo='#02 - Com parâmetro' color='#E8B71A'>
+                    <WithParameter 
                         titulo='Situação do aluno' 
                         aluno='Herique' 
                         nota={10}/>
                 </Card>
-                <Card titulo='#01 - Primeiro Componente'>
-                    <Primeiro></Primeiro>
+                <Card titulo='#01 - Primeiro Componente' color='#588c73'>
+                    <First></First>
                 </Card>
             </div>
         </div>
