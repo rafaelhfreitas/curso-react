29dafc46622e80d56fd700110fd7a4cc3e3e35d1
Rafael Freitas <oraffaudi@gmail.com>
2020-09-20 15:56:44 -0300

    New comp counter

---
+++ src/components/counter/Buttons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default (props) => {
+    return (
+        <div>
+            <button onClick={props.setIncrement}>+</button>
+            <button onClick={props.setDecrement}>-</button>            
+        </div>
+    )
+
+}
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,17 +1,13 @@
 import './Counter.css'
 import React, { Component } from 'react'
 
+
+import Display from './Display'
+import Buttons from './Buttons'
+import Step from './StepForm'
+import StepForm from './StepForm'
+
 class Counter extends Component {
-
-    // constructor(props) {
-    //     super(props)
-
-    //     this.state = {
-    //         number: props.initialNumber
-    //     }
-    
-    // }
-
 
     state = {
         number: this.props.initialNumber || 0,
@@ -32,35 +28,61 @@ class Counter extends Component {
          
     }
 
-    decrement() {
+    // decrement() {
+    //     this.setState({
+    //         number: this.state.number - this.state.step
+    //     })
+    // }
+
+    decrement = () => {
         this.setState({
             number: this.state.number - this.state.step
         })
     }
 
 
-    setStep = (event) => {
+    // setStep = (event) => {
+    //     this.setState({
+    //         step: +event.target.value
+    //     })
+    // }
+
+    setStep = (newStep) => {
         this.setState({
-            step: +event.target.value
+            step: newStep
         })
     }
+
+    // render() {
+    //     return (
+    //         <div className="Counter">
+    //              <h2 >Contador</h2>
+    //              <h3>{this.state.number}</h3>
+    //              <div>
+    //                  <label htmlFor="stepInput">Step: </label>
+    //                  <input 
+    //                     id="stepInput" 
+    //                     type="number" 
+    //                     value={this.state.step} 
+    //                     onChange={this.setStep} />    
+    //              </div>
+    //              <button onClick={this.increment}>+</button>
+    //              <button onClick={_ => this.decrement()}>-</button>
+    //         </div>
+    //     )
+    // }
+
 
     render() {
         return (
             <div className="Counter">
                  <h2 >Contador</h2>
-                 <h3>{this.state.number}</h3>
-                 <div>
-                     <label htmlFor="stepInput">Step: </label>
-                     <input 
-                        id="stepInput" 
-                        type="number" 
-                        value={this.state.step} 
-                        onChange={this.setStep} />    
-                 </div>
-                 <button onClick={this.increment}>+</button>
-                 <button onClick={_ => this.decrement()}>-</button>
-                 <p>Número inicial: {this.state.number}</p>
+                 <Display number={this.state.number}></Display>
+                 <StepForm step={this.state.step} setStep={this.setStep}></StepForm>
+                 <Buttons 
+                    setIncrement={this.increment} 
+                    setDecrement={this.decrement}
+                />
             </div>
         )
     }
